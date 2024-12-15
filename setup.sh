bd4fad8b056f37c8f9a81a5b117cf8ef2aaee2df
WeTALKUPC <wetalkupc@gmail.com>
2024-12-15 00:48:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Actualizar pip antes de instalar cualquier otra dependencia
+# Actualizar el sistema e instalar dependencias del sistema necesarias para Pillow
+sudo apt-get update
+sudo apt-get install -y zlib1g-dev
+
+# Actualizar pip a la última versión
 pip install --upgrade pip
+
+# Instalar las dependencias de Python especificadas en requirements.txt
+pip install -r requirements.txt
